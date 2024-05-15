5e2d70387d3660fa2eafff78b5e2b44eb9698d25
Mohamed Ibraheem <mohamedibraheem509@gmail.com>
2024-05-15 23:52:19 +0300

    Refactor

---
+++ src/http_server/mod.rs
@@ -0,0 +1,205 @@
+use std::borrow::Cow;
+use std::collections::HashMap;
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+
+#[derive(Debug, PartialEq)]
+pub enum Method {
+    GET,
+}
+
+#[derive(Debug)]
+struct Handler {
+    pub handler: fn(Request, Response),
+    pub path: String,
+    pub method: Method,
+}
+
+#[derive(Debug)]
+pub struct Application {
+    pub port: u16,
+    stream: Option<TcpStream>,
+    handlers: Vec<Handler>,
+}
+
+impl Application {
+    pub fn new(port: u16) -> Application {
+        Application {
+            port,
+            stream: None,
+            handlers: vec![],
+        }
+    }
+
+    pub fn listen(mut self, handler: fn(&Application)) {
+        let listener = TcpListener::bind(format!("127.0.0.1:{}", self.port)).unwrap();
+        handler(&self);
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    self.stream = Some(stream);
+                    self.route();
+                }
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                }
+            }
+        }
+    }
+
+    pub fn get(&mut self, path: &str, handler: fn(Request, Response)) {
+        self.handlers.push(Handler {
+            path: path.to_string(),
+            handler,
+            method: Method::GET,
+        });
+    }
+
+    fn route(&mut self) {
+        let mut buf = [0; 1024];
+        self.stream.as_ref().unwrap().read(&mut buf).unwrap();
+
+        let (method, path) = Application::parse_request_line(&String::from_utf8_lossy(&buf[..]));
+
+        let mut response = Response::new(200, self.stream.as_mut().unwrap());
+        match method.as_str() {
+            "GET" => {
+                let request = Request::new(Method::GET, path.to_string());
+                let gets = self
+                    .handlers
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, h)| h.method == Method::GET)
+                    .map(|(_, h)| h)
+                    .collect::<Vec<_>>();
+
+                Application::router(gets, &path, request, response);
+            }
+            _ => {
+                response.code(404).send();
+            }
+        }
+    }
+
+    fn parse_request_line(request: &Cow<str>) -> (String, String) {
+        let request_line = request
+            .lines()
+            .next()
+            .unwrap()
+            .split(" ")
+            .collect::<Vec<_>>();
+
+        (request_line[0].to_string(), request_line[1].to_string())
+    }
+
+    fn router(handlers: Vec<&Handler>, path: &str, mut request: Request, mut response: Response) {
+        for hd in handlers {
+            let re = Application::parse_path(&hd.path);
+            if re.is_match(&path) {
+                let captures = re.captures(&path).unwrap();
+                request.params = re
+                    .capture_names()
+                    .flatten()
+                    .filter_map(|x| Some((x.to_string(), captures.name(x)?.as_str().to_string())))
+                    .collect::<HashMap<String, String>>();
+                (hd.handler)(request, response);
+                return;
+            }
+        }
+        response.code(404).send();
+    }
+
+    fn parse_path(path: &str) -> regex::Regex {
+        let mut regex_str = "^".to_string();
+        if path == "/" {
+            if path.starts_with("/:") {
+                regex_str.push_str(&format!(r"/(?P<{}>\w+)", path.get(2..).unwrap()))
+            } else {
+                regex_str.push_str("/")
+            }
+        } else {
+            for str in path.split("/") {
+                if !str.is_empty() {
+                    if str.starts_with(":") {
+                        regex_str.push_str(&format!(r"/(?P<{}>\w+)", str.get(1..).unwrap()))
+                    } else {
+                        regex_str.push_str(&format!("/{}", str))
+                    }
+                }
+            }
+        }
+        regex_str.push_str("$");
+        regex::Regex::new(&regex_str).unwrap()
+    }
+}
+
+#[derive(Debug)]
+pub struct Request {
+    pub method: Method,
+    pub path: String,
+    pub params: HashMap<String, String>,
+}
+
+impl Request {
+    pub fn new(method: Method, path: String) -> Request {
+        Request {
+            method,
+            path,
+            params: HashMap::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Response<'a> {
+    code: u16,
+    stream: &'a mut TcpStream,
+}
+
+impl Response<'_> {
+    pub fn new(code: u16, stream: &mut TcpStream) -> Response {
+        Response { code, stream }
+    }
+
+    pub fn code(&mut self, code: u16) -> &mut Self {
+        self.code = code;
+        self
+    }
+
+    pub fn send_text(&mut self, data: &str) {
+        self.write_res_code();
+        self.stream
+            .write("Content-Type: text/plain\r\n".as_bytes())
+            .unwrap();
+        self.stream
+            .write(format!("Content-Length: {}\r\n", data.len()).as_bytes())
+            .unwrap();
+        self.stream.write("\r\n".as_bytes()).unwrap();
+        self.stream.write(data.as_bytes()).unwrap();
+        self.stream.shutdown(Shutdown::Both).unwrap();
+    }
+
+    pub fn send(&mut self) {
+        self.write_res_code();
+        self.stream.write("\r\n".as_bytes()).unwrap();
+        self.stream.shutdown(Shutdown::Both).unwrap();
+    }
+
+    fn write_res_code(&mut self) {
+        match self.code {
+            200 => {
+                self.stream.write("HTTP/1.1 200 OK\r\n".as_bytes()).unwrap();
+            }
+            404 => {
+                self.stream
+                    .write("HTTP/1.1 404 Not Found\r\n".as_bytes())
+                    .unwrap();
+            }
+            500 | _ => {
+                self.stream
+                    .write("HTTP/1.1 500 Internal Server Error\r\n".as_bytes())
+                    .unwrap();
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,26 @@
-use std::borrow::Cow;
-use std::io::{Read, Write};
-use std::net::{Shutdown, TcpListener, TcpStream};
+mod http_server;
+
+use std::env;
+
+use crate::http_server::Application;
 
 fn main() {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("accepted new connection");
-                handle_connection(&stream);
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
-    }
-}
+    let args = env::args().collect::<Vec<String>>();
+    println!("{:?}", args);
 
-fn handle_connection(mut stream: &TcpStream) {
-    let mut buf = [0; 1024];
-    stream.read(&mut buf).unwrap();
+    let mut app = Application::new(4221);
 
-    let request = String::from_utf8_lossy(&buf[..]);
-    let (method, path) = parse_request_line(&request);
+    app.get("/", |_request, mut response| {
+        response.code(200).send();
+    });
 
-    match method.as_str() {
-        "GET" => get_path(&stream, &request, path.as_str()),
-        _ => {
-            response(&stream, 500);
-        }
-    }
-}
+    app.get("/echo/:str", |request, mut response| {
+        response.send_text(&request.params["str"]);
+    });
 
-fn get_path(stream: &TcpStream, request: &Cow<str>, path: &str) {
-    match path {
-        "/" => {
-            response(&stream, 200);
-        }
-        p if p.starts_with("/echo") => {
-            let data = p.split("/").collect::<Vec<_>>()[2];
-            response_with_data(&stream, 200, data);
-        }
-        "/user-agent" => {
-            let user_agent = get_header(&request, "User-Agent");
-            response_with_data(&stream, 200, user_agent.unwrap().as_str());
-        }
-        _ => {
-            response(&stream, 404);
-        }
-    }
-}
-
-fn parse_request_line(request: &Cow<str>) -> (String, String) {
-    let request_line = request
-        .lines()
-        .next()
-        .unwrap()
-        .split(" ")
-        .collect::<Vec<_>>();
-
-    (request_line[0].to_string(), request_line[1].to_string())
-}
-
-fn get_header(request: &Cow<str>, header: &str) -> Option<String> {
-    request
-        .lines()
-        .find(|line| line.starts_with(header))
-        .map(|line| line.split(": ").collect::<Vec<_>>()[1].to_string())
-}
-
-fn write_res_code(mut stream: &TcpStream, code: u16) {
-    match code {
-        200 => {
-            stream.write("HTTP/1.1 200 OK\r\n".as_bytes()).unwrap();
-        }
-        404 => {
-            stream
-                .write("HTTP/1.1 404 Not Found\r\n".as_bytes())
-                .unwrap();
-        }
-        500 | _ => {
-            stream
-                .write("HTTP/1.1 500 Internal Server Error\r\n".as_bytes())
-                .unwrap();
-        }
-    }
-}
-
-fn response_with_data(mut stream: &TcpStream, code: u16, data: &str) {
-    write_res_code(stream, code);
-    stream
-        .write("Content-Type: text/plain\r\n".as_bytes())
-        .unwrap();
-    stream
-        .write(format!("Content-Length: {}\r\n", data.len()).as_bytes())
-        .unwrap();
-    stream.write("\r\n".as_bytes()).unwrap();
-    stream.write(data.as_bytes()).unwrap();
-    stream.shutdown(Shutdown::Both).unwrap();
-}
-
-fn response(mut stream: &TcpStream, code: u16) {
-    write_res_code(stream, code);
-    stream.write("\r\n".as_bytes()).unwrap();
-    stream.shutdown(Shutdown::Both).unwrap();
+    app.listen(|app| {
+        println!("Server run on port: {}", app.port);
+    });
 }
